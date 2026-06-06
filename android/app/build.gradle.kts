dfd78d2678a4148ff3d05de3def6ffe4a3921b32
chiragdhunna <chiragdhunna2468@gmail.com>
2026-06-07 02:19:27 +0530

    style(android): clean up redundant comments
    Remove default generated TODO and explanatory comments from android/app/build.gradle.kts, and fix the trailing newline in the build file.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
     // END: FlutterFire Configuration
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -18,10 +17,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.pesa_barbaadi"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = (System.getenv("BUILD_NUMBER") ?: "1").toInt()
